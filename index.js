c7acff47fff69dc1be2ff2d86eeb0fc9bb6a4dac
DinisValentim <dinisvalentim1504@gmail.com>
2026-01-16 16:12:32 +0100

    Mankey Added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 class Pokemon{
+
+  static AllPokemons = []
+
   constructor(own,name,weak,resistance,hp,IdAttacks){
     this.own = own
     this.name = name
@@ -7,11 +10,12 @@ class Pokemon{
     this.resistance = resistance
     this.hp = hp
     this.IdAttacks = IdAttacks 
+    Pokemon.AllPokemons.push(this)
   }
   ChooseAttacks(Pokemon){
     let Attacks = []
     Pokemon.IdAttacks.forEach((IdAttacks) => {
-      AllAttacks.forEach((attack) => {
+      Attack.AllAttacks.forEach((attack) => {
         if (attack.IdAttacks == IdAttacks){
           Attacks.push(attack)
         }
@@ -21,62 +25,61 @@ class Pokemon{
   }
 }
 class Attack{
+
+  static AllAttacks = []
+
   constructor(IdAttacks,name,type,dmg){
     this.IdAttacks = IdAttacks
     this.name = name
     this.type = type
     this.dmg = dmg
+    Attack.AllAttacks.push(this)
   }
 }
-// Pokemons
-Bulbasaur = new Pokemon(0,'Bulbasaur',["Fire","Flying","Psychic","Ice"],["Water","Fightin","Grass","Eletric","Fairy"],36,[1,2,3,4])
-Charmander = new Pokemon(0,'Charmander',["Water","Rock","Ground"],["Grass","Bug","Steel","Fire","Ice","Fairy"],37,[1,5,6,7])
-Squirtle = new Pokemon(0,'Squirtle',["Grass","Eletric"],["Fire","Steel","Water","Ice"],38,[1,8,9,10])
-Pikachu = new Pokemon(0,'Pikachu',["Ground"],["Steel","Eletric","Flying"],34,[1,11,12,13])
-Butterfree = new Pokemon(0,'Butterfree',["Flying","Rock","Fire","Eletric","Ice"],["Fightin","Ground","Bug","Grass"],42,[14,15,16,17])
-Beedrill = new Pokemon(0,'Beedrill',["Flying","Rock","Fire","Psychic"],["Fightin","Poison","Bug","Grass","Fairy"],38,[20,19,18,21])
-Pidgeot = new Pokemon(0,'Pidgeot',["Rock","Eletric","Ice"],["Ground","Bug","Grass","Ghost"],38,[14,12,22,23])
-Rattata = new Pokemon(0,'Rattata',["Fightin"],["Ghost"],33,[1,12,8,24])
-Eevee = new Pokemon(0,'Eevee',["Fightin"],["Ghost"],35,[1,12,25,24])
-Mankey = new Pokemon(0,'Mankey',["Flying","Psychic","Fairy"],["Ghost"],37,[1,26,28,27])
-
-const palbasaur = new Pokemon(0,'Bulbasaur',["Fire","Flying","Psychic","Ice"],["Water","Fightin","Grass","Eletric","Fairy"],36,[1,2,3,4])
-
-// Attacks
-Tackle = new Attack(1,'Tackle','Normal',6)
-Razor_Leaf = new Attack(2,'Razor Leaf','Grass',10)
-Vine_Whip = new Attack(3,'Vine Whip','Grass',8)
-Sludge_Wave = new Attack(4,'Sludge Wave','Poison',9)
-Dragon_Breath = new Attack(5,'Dragon Breath','Dragon',9 )
-Ember = new Attack(6,'Ember','Fire',10)
-Slash = new Attack(7,'Slash','Normal',7)
-Bite = new Attack(8,'Bite','Dark',9)
-Aqua_Beam = new Attack(9,'Aqua Beam','Water',10)
-Aqua_jet = new Attack(10,'Aqua Jet','Water',8)
-Volt_Tackle = new Attack(11,'Volt Tackle','Eletric',12)
-Quick_Attack = new Attack(12,'Quick Attack','Normal',8)
-Spark = new Attack(13,'Spark','Eletric',10)
-Gust = new Attack(14,'Gust','Flying',9)
-Bug_Bite = new Attack(15,'Bug Bite','Bug',9)
-Confusion = new Attack(16,'Confusion','Psychic',11)
-Air_Slash = new Attack(17,'Air Slash','Flying',12)
-Brutal_Swig = new Attack(18,'Brutal Swig','Dark',12)
-Bug_Buzz = new Attack(19,'Bug Buzz','Bug',11)
-Drill_Run = new Attack(20,'Drill Run','Ground',11)
-Poison_Sting = new Attack(21,'Poison Sting','Poison',8)
-Twister = new Attack(22,'Twister','Dragon',9)
-Brave_Bird = new Attack(23,'Brave Bird','Flying',13)
-Take_Down = new Attack(24,'Take Down','Normal',15)
-Disarming_Voice = new Attack(25,'Disarming Voice','Fairy',9)
-Rock_Throw = new Attack(26,'Rock Throw','Fairy',9)
-Close_Combat = new Attack(27,'Close Combat','Fairy',12)
-Facade = new Attack(28,'Facade','Fairy',10)
+// Pokemons 10
+const Bulbasaur = new Pokemon(0,'Bulbasaur',["Fire","Flying","Psychic","Ice"],["Water","Fighting","Grass","Eletric","Fairy"],36,[1,2,3,4])
+const Charmander = new Pokemon(0,'Charmander',["Water","Rock","Ground"],["Grass","Bug","Steel","Fire","Ice","Fairy"],37,[1,5,6,7])
+const Squirtle = new Pokemon(0,'Squirtle',["Grass","Eletric"],["Fire","Steel","Water","Ice"],38,[1,8,9,10])
+const Pikachu = new Pokemon(0,'Pikachu',["Ground"],["Steel","Eletric","Flying"],34,[1,11,12,13])
+const Butterfree = new Pokemon(0,'Butterfree',["Flying","Rock","Fire","Eletric","Ice"],["Fighting","Ground","Bug","Grass"],42,[14,15,16,17])
+const Beedrill = new Pokemon(0,'Beedrill',["Flying","Rock","Fire","Psychic"],["Fighting","Poison","Bug","Grass","Fairy"],38,[20,19,18,21])
+const Pidgeot = new Pokemon(0,'Pidgeot',["Rock","Eletric","Ice"],["Ground","Bug","Grass","Ghost"],38,[14,12,22,23])
+const Rattata = new Pokemon(0,'Rattata',["Fighting"],["Ghost"],33,[1,12,8,24])
+const Eevee = new Pokemon(0,'Eevee',["Fighting"],["Ghost"],35,[1,12,25,24])
+const Mankey = new Pokemon(0,'Mankey',["Flying","Psychic","Fairy"],["Dark","Bug","Rock"],37,[1,26,28,27])
+// Attacks 28
+const Tackle = new Attack(1,'Tackle','Normal',6)
+const Razor_Leaf = new Attack(2,'Razor Leaf','Grass',10)
+const Vine_Whip = new Attack(3,'Vine Whip','Grass',8)
+const Sludge_Wave = new Attack(4,'Sludge Wave','Poison',9)
+const Dragon_Breath = new Attack(5,'Dragon Breath','Dragon',9 )
+const Ember = new Attack(6,'Ember','Fire',10)
+const Slash = new Attack(7,'Slash','Normal',7)
+const Bite = new Attack(8,'Bite','Dark',9)
+const Aqua_Beam = new Attack(9,'Aqua Beam','Water',10)
+const Aqua_jet = new Attack(10,'Aqua Jet','Water',8)
+const Volt_Tackle = new Attack(11,'Volt Tackle','Eletric',12)
+const Quick_Attack = new Attack(12,'Quick Attack','Normal',8)
+const Spark = new Attack(13,'Spark','Eletric',10)
+const Gust = new Attack(14,'Gust','Flying',9)
+const Bug_Bite = new Attack(15,'Bug Bite','Bug',9)
+const Confusion = new Attack(16,'Confusion','Psychic',11)
+const Air_Slash = new Attack(17,'Air Slash','Flying',12)
+const Brutal_Swig = new Attack(18,'Brutal Swig','Dark',12)
+const Bug_Buzz = new Attack(19,'Bug Buzz','Bug',11)
+const Drill_Run = new Attack(20,'Drill Run','Ground',11)
+const Poison_Sting = new Attack(21,'Poison Sting','Poison',8)
+const Twister = new Attack(22,'Twister','Dragon',9)
+const Brave_Bird = new Attack(23,'Brave Bird','Flying',13)
+const Take_Down = new Attack(24,'Take Down','Normal',15)
+const Disarming_Voice = new Attack(25,'Disarming Voice','Fairy',9)
+const Rock_Throw = new Attack(26,'Rock Throw','Rock',9)
+const Close_Combat = new Attack(27,'Close Combat','Fighting',12)
+const Facade = new Attack(28,'Facade','Normal',10)
 
 
 
 StarterPokemons = [Bulbasaur,Charmander,Squirtle]
-AllPokemons = [Bulbasaur,Charmander,Squirtle,Pikachu,Butterfree,Beedrill,Pidgeot,Rattata,Eevee]
-AllAttacks = [Tackle,Razor_Leaf,Vine_Whip,Sludge_Wave,Dragon_Breath,Ember,Slash,Bite,Aqua_Beam,Aqua_jet,Volt_Tackle,Quick_Attack,Spark,Gust,Bug_Bite,Confusion,Air_Slash,Brutal_Swig,Bug_Buzz,Drill_Run,Poison_Sting,Twister,Brave_Bird,Take_Down,Disarming_Voice,Rock_Throw,Close_Combat,Facade]
 UserPokemon = []
 UserAttacks = []
 WildPokemonAttacks = []
@@ -137,18 +140,18 @@ function Menu(){
 //Battle
 function Battle(){
   let owned = []
-  AllPokemons.forEach((pokemon)=>{
+  Pokemon.AllPokemons.forEach((pokemon)=>{
     if(pokemon.own == 1)
       owned.push(pokemon)
   })
-  if(owned.length == AllPokemons.length){
+  if(owned.length == Pokemon.AllPokemons.length){
     console.log(" ")
     console.log("You captured every pokemon in this area.")
     console.log("There's no more Pokemons left to Battle.")
     console.log(" ")
     setTimeout(() => Menu(), 2000)
   }else{
-    wildPokemon=AllPokemons[randomInt(AllPokemons.length - 0)]
+    wildPokemon=Pokemon.AllPokemons[randomInt(Pokemon.AllPokemons.length - 0)]
     if (wildPokemon.own == 1){
       Battle()
     }else{
@@ -233,9 +236,9 @@ function Capture(){
       CapturePokemon = randomInt(4 - 0)
       if(CapturePokemon == 1 ){
         console.log("You captured the wild Pokemon successfully!")
-        AllPokemons.forEach((pokemon, index)=>{
+        Pokemon.AllPokemons.forEach((pokemon, index)=>{
           if(pokemon.name == wildPokemon.name)
-            AllPokemons[index].own=1
+            Pokemon.AllPokemons[index].own=1
         })
         setTimeout(() => Menu(), 2000)
       }else{
@@ -249,7 +252,7 @@ function Capture(){
 //Owned Pokemons
 function OwnedPokemons(){
   let owned = []
-  AllPokemons.forEach((pokemon)=>{
+  Pokemon.AllPokemons.forEach((pokemon)=>{
     if(pokemon.own == 1)
       owned.push(pokemon)
   })
@@ -277,7 +280,7 @@ function TypeOfDamage(Who, Attack) {
   if(Who == "User"){
     wildPokemon.weak.forEach((types)=>{
       if(types == UserAttacks[Attack-1].type){
-        damage = UserAttacks[Attack-1].dmg * 1.5
+        damage = UserAttacks[Attack-1].dmg * 2
         wildPokemon.hp = wildPokemon.hp - damage
         console.log("The attack was very effective.")
         console.log("It did " + damage + " damage to the wild Pokemon")
@@ -302,7 +305,7 @@ function TypeOfDamage(Who, Attack) {
   }else{
     UserPokemon.weak.forEach((type)=>{
       if(type == wildUsed.type){
-        damage = wildUsed.dmg * 1.5
+        damage = wildUsed.dmg * 2
         UserPokemon.hp = UserPokemon.hp - damage
         console.log("The attack was very effective.")
         console.log("It did " + damage + " damage to your Pokemon")
@@ -339,4 +342,3 @@ function Verify(valor) {
   }
 }
 
-console.log("Thank you for playing!")
